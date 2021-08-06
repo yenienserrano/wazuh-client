d05e2e67a613e54aa159f8ad277f405ae0c1fb17
Ian Serrano <yenienserrano75@gmail.com>
2021-08-06 13:01:21 +0200

    agent page

--- src/pages/Agent.jsx
+++ src/pages/Agent.jsx
@@ -1,9 +1,115 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { AlertDetail } from "../components/alerts/AlertDetail";
+import { AlertTable } from "../components/alerts/AlertTable";
 
 export const Agent = () => {
+  const [agent, setAgent] = useState([]);
+  const [alert, setAlert] = useState(null);
+  const [alerts, setAlerts] = useState(null);
+  const [tenAlerts, setTenAlerts] = useState(null);
+  const [show, setShow] = useState(false);
+  const [showMore, setShowMore] = useState(false);
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const res = await fetch(`${process.env.REACT_APP_API_URL}/agents/${id}`);
+    const data = await res.json();
+    setAgent(data.data);
+    setAlerts(data.data.alerts);
+    setTenAlerts(data.data.alerts.slice(0, 10));
+  };
+
+  const alertDetail = (alert) => {
+    setAlert(alert);
+    setShow(true);
+  };
+
   return (
-    <div>
-      <h1>Hola desde agent</h1>
-    </div>
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <h1 className="text-white mt-4 mb-4">Agent details</h1>
+            <div className="card bg-dark text-white">
+              <div className="card-header">
+                {" "}
+                <b> Agente:</b> {agent.name}
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item bg-dark text-white">
+                  <b> Id:</b> {agent.id}
+                </li>
+                <li className="list-group-item bg-dark text-white">
+                  <b> Ip:</b> {agent.ip}
+                </li>
+                <li className="list-group-item bg-dark text-white">
+                  <b> Total alerts:</b> {agent.total_alerts}
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-md-8">
+            <h1 className="text-white mt-4 mb-4">Agent alerts</h1>
+
+            <table className="table table-dark table-striped table-hover">
+              <thead>
+                <tr>
+                  <th scope="col">Id</th>
+                  <th scope="col">Agent</th>
+                  <th scope="col">Rule</th>
+                  <th scope="col">Event</th>
+                </tr>
+              </thead>
+              <tbody>
+                {alerts &&
+                  showMore &&
+                  alerts.map((alert) => (
+                    <AlertTable
+                      key={alert._id}
+                      alert={alert}
+                      alertDetail={alertDetail}
+                    />
+                  ))}
+                {tenAlerts &&
+                  !showMore &&
+                  tenAlerts.map((alert) => (
+                    <AlertTable
+                      key={alert._id}
+                      alert={alert}
+                      alertDetail={alertDetail}
+                    />
+                  ))}
+              </tbody>
+            </table>
+            <div className="d-flex justify-content-center">
+              {alerts && alerts.length > 10 ? (
+                showMore ? (
+                  <button
+                    className="btn btn-dark"
+                    onClick={() => setShowMore(false)}
+                  >
+                    See less
+                  </button>
+                ) : (
+                  <button
+                    className="btn btn-dark"
+                    onClick={() => setShowMore(true)}
+                  >
+                    See all
+                  </button>
+                )
+              ) : null}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {show && <AlertDetail setShow={setShow} alert={alert} />}
+    </>
   );
 };
